43ff1072fa09e1e1d2754ddeb90bc7bfe215de9a
MarieDeSmedt <desmedtmarie@outlook.com>
2021-12-15 16:38:56 +0100

    create pages 1,2 and 3

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-# test
+
+import streamlit as st
+
+# Custom imports 
+from multipage import MultiPage
+from pages import page1
+
+# Create an instance of the app 
+app = MultiPage()
+
+# Title of the main page
+st.title("Data Storyteller Application")
+
+# Add all your applications (pages) here
+app.add_page("Page n°1", page1.display)
+app.add_page("Page n°2", page1.display)
+app.add_page("Page n°3", page1.display)
+
+
+# The main app
+app.run()
+
