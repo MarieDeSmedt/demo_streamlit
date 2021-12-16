26dcb74fae343b35d3ec82ff48fb80959bcb9333
MarieDeSmedt <desmedtmarie@outlook.com>
2021-12-16 15:14:50 +0100

    correct slider

--- pages/page2.py
+++ pages/page2.py
@@ -6,25 +6,27 @@ import pandas as pd
 
 
 def display():
-    simple, style, table = display_sidebar()
+    nb_rows, simple, style, table = display_sidebar()
     display_header()
-    display_body(simple, style, table)
+    display_body(nb_rows, simple, style, table)
     
 
 def display_sidebar():
+    st.sidebar.subheader('Combien de lignes voulez vous?')
+    nb_rows = st.sidebar.slider('',5,30)
     simple =st.sidebar.checkbox('affichage dataframe simple', value=True)
     style =st.sidebar.checkbox('affichage avec style')
     table =st.sidebar.checkbox('affichage sous forme de table')
     
     st.sidebar.button('bouton page3')   
-    return simple, style, table
+    return nb_rows,simple, style, table
     
 def display_header():
     st.title("page 3")
 
-def display_body(simple, style, table):
+def display_body(nb_rows,simple, style, table):
     data = pd.read_csv("CSV/Streamlit_test.csv")
-    dataframe = data.head(5)
+    dataframe = data.head(nb_rows)
     if simple:
         st.write('affichage dataframe simple')
         st.dataframe(dataframe)
